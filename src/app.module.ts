690acaf9394b1090fc287686ec4b8d4e08b2aa84
RonaldGuilhermePDS <RonaldGuilhermePDS@gmail.com>
2024-03-31 14:22:48 -0300

    chore: add health route

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './application/app.controller';
 import { AppService } from './application/app.service';
+import { HealthController } from './application/health.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, HealthController],
   providers: [AppService],
 })
 export class AppModule {}
