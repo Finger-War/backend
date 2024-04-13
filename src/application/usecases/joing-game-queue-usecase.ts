d140d32bb4695e3cff3cf719edc220d5b52f261a
RonaldGuilhermePDS <RonaldGuilhermePDS@gmail.com>
2024-04-13 16:50:39 -0300

    feat: ensure joing game use case not add player to the game queue if player already exists

---
+++ src/application/usecases/joing-game-queue-usecase.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+
+import { JoinGameQueue } from '@/domain/usecases/join-game-queue-usecase';
+
+import { GameService } from '@/infrastructure/services/game-service';
+
+@Injectable()
+export class JoingGameQueueUseCase implements JoinGameQueue {
+  constructor(private readonly gameService: GameService) {}
+
+  execute(playerId: string) {
+    const playerAlreadyExistsInQueue = this.gameService.getPlayer(playerId);
+
+    if (playerAlreadyExistsInQueue) {
+      return;
+    }
+
+    this.gameService.joinQueue(playerId);
+  }
+}
