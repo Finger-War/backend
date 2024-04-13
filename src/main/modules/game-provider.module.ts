99611c879ca6fe199abf4d4e74f6b59cf26148a6
RonaldGuilhermePDS <RonaldGuilhermePDS@gmail.com>
2024-04-13 17:07:13 -0300

    feat: add game controller

--- src/main/modules/game-provider.module.ts
+++ src/main/modules/game-provider.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 
+import { JoingGameQueueUseCase } from '@/application/usecases/joing-game-queue-usecase';
+
+import { GameController } from '@/presentation/controllers/game-controller';
+
+import { GameService } from '@/infrastructure/services/game-service';
+
 import { GameProvider } from '@/infrastructure/providers/game-provider';
 
 @Module({
   imports: [],
-  providers: [GameProvider],
+  providers: [GameController, JoingGameQueueUseCase, GameService, GameProvider],
 })
 export class GameProviderModule {}
---
+++ src/presentation/controllers/game-controller.ts
@@ -0,0 +1,18 @@
+import { Socket } from 'socket.io';
+import { WebSocketGateway, SubscribeMessage } from '@nestjs/websockets';
+
+import { JoingGameQueueUseCase } from '@/application/usecases/joing-game-queue-usecase';
+
+export interface IGameController {
+  handleStart(client: Socket): void;
+}
+
+@WebSocketGateway(5000, { cors: true })
+export class GameController implements IGameController {
+  constructor(private readonly joinGameQueueUseCase: JoingGameQueueUseCase) {}
+
+  @SubscribeMessage('join-queue')
+  handleStart(client: Socket): void {
+    this.joinGameQueueUseCase.execute(client.id);
+  }
+}
