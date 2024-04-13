57c268c5539ad2e3f3ab55199961998a904095f0
RonaldGuilhermePDS <RonaldGuilhermePDS@gmail.com>
2024-04-13 16:24:47 -0300

    feat: ensure game service returns an empty object if the queue has less than 2 players

--- src/infrastructure/services/game-service.ts
+++ src/infrastructure/services/game-service.ts
@@ -6,13 +6,12 @@ export interface IGameService {
   joinQueue(playerId: string): void;
   getOutQueue(playerId: string): void;
   getPlayer(playerId: string): Player | null;
+  getPlayers(): Record<string, Player>;
 }
 
 @Injectable()
 export class GameService implements IGameService {
   private queue: Player[] = [];
-
-  constructor() {}
 
   joinQueue(playerId: string): void {
     const player = { id: playerId };
@@ -26,5 +25,16 @@ export class GameService implements IGameService {
 
   getPlayer(playerId: string): Player | null {
     return this.queue.find((player) => player.id === playerId) ?? null;
+  }
+
+  getPlayers(): Record<string, Player> {
+    if (this.queue.length < 2) {
+      return {};
+    }
+
+    const playerOne = this.queue.shift();
+    const playerTwo = this.queue.shift();
+
+    return { playerOne, playerTwo };
   }
 }
