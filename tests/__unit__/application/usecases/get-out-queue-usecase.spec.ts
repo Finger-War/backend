5ab5248b779e1603b3172609f7b0b8287d159733
RonaldGuilhermePDS <RonaldGuilhermePDS@gmail.com>
2024-04-18 19:24:10 -0300

    refactor: move sut creation to a factory method

--- tests/__unit__/application/usecases/get-out-queue-usecase.spec.ts
+++ tests/__unit__/application/usecases/get-out-queue-usecase.spec.ts
@@ -1,20 +1,27 @@
 import { GetOutQueueUseCase } from '@/application/usecases/get-out-queue-usecase';
 import { GameService } from '@/infrastructure/services/game-service';
 
+const makeSut = () => {
+  const gameService = new GameService();
+  const sut = new GetOutQueueUseCase(gameService);
+
+  return { sut, gameService };
+};
+
 describe('Get Out Queue Use Case', () => {
   it('Should not remove player from the game queue if player does not exist', () => {
+    const { sut } = makeSut();
+
     const playerId = '1';
-    const gameService = new GameService();
-    const sut = new GetOutQueueUseCase(gameService);
 
     expect(sut.execute(playerId)).toBeUndefined();
   });
 
   it('Should remove player from the game queue if player exist', () => {
+    const { sut, gameService } = makeSut();
+
     const playerId = '1';
-    const gameService = new GameService();
     gameService.joinQueue(playerId);
-    const sut = new GetOutQueueUseCase(gameService);
 
     sut.execute(playerId);
 
