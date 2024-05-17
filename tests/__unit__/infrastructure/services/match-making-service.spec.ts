1bda2d2e83f58279d0d3feb9ec34bb089dadca99
RonaldGuilhermePDS <RonaldGuilhermePDS@gmail.com>
2024-05-17 00:33:11 -0300

    test: ensure match making service starts a match if there is a match available

---
+++ tests/__unit__/infrastructure/services/match-making-service.spec.ts
@@ -0,0 +1,57 @@
+import { Test } from '@nestjs/testing';
+
+import { InMemoryMatchRepository } from '@/infrastructure/repositories/in-memory-match-repository';
+import { GameService } from '@/infrastructure/services/game-service';
+import { MatchMakingService } from '@/infrastructure/services/match-making-service';
+import { GameConstants } from '@/main/constants/game-constants';
+import { Server } from 'socket.io';
+
+const makeSut = async () => {
+  const moduleRef = await Test.createTestingModule({
+    providers: [
+      GameService,
+      { provide: InMemoryMatchRepository, useClass: InMemoryMatchRepository },
+      MatchMakingService,
+    ],
+  }).compile();
+
+  const gameService = moduleRef.get<GameService>(GameService);
+
+  const sut = moduleRef.get<MatchMakingService>(MatchMakingService);
+
+  return { sut, gameService };
+};
+
+describe('Match Making Service', () => {
+  describe('handle', () => {
+    it('Should start a match if there is a match available', async () => {
+      const { sut, gameService } = await makeSut();
+
+      const playerOne = { id: 'player1' };
+      const playerTwo = { id: 'player2' };
+      const roomId = `match:${playerOne.id}-${playerTwo.id}`;
+
+      vitest
+        .spyOn(gameService, 'tryMatch')
+        .mockReturnValue([playerOne, playerTwo]);
+
+      const server: Server = {
+        sockets: {
+          sockets: {
+            get: vitest.fn().mockReturnValue({ join: vitest.fn() }),
+          },
+        },
+        to: vitest.fn().mockReturnValue({ emit: vitest.fn() }),
+      } as any;
+
+      sut.handle(server);
+
+      expect(server.sockets.sockets.get).toHaveBeenCalledWith(playerOne.id);
+      expect(server.sockets.sockets.get).toHaveBeenCalledWith(playerTwo.id);
+      expect(server.to).toHaveBeenCalledWith(roomId);
+      expect(server.to(roomId).emit).toHaveBeenCalledWith(
+        GameConstants.client.matchStart,
+      );
+    });
+  });
+});
