f540b29cd700d208d8ec098710b24febe2b3a093
RonaldGuilhermePDS <RonaldGuilhermePDS@gmail.com>
2024-05-23 19:29:31 -0300

    test: ensure handle word use case not emit if there is no adversary in the room

--- tests/__unit__/application/usecases/handle-word-usecase.spec.ts
+++ tests/__unit__/application/usecases/handle-word-usecase.spec.ts
@@ -43,4 +43,22 @@ describe('Handle Word Use Case', () => {
 
     expect(client.to).not.toHaveBeenCalled();
   });
+
+  it('Should not emit if there is no adversary in the room', async () => {
+    const { sut } = await makeSut();
+
+    const client = makeClientMock();
+    const server = makeServerMock();
+
+    client.rooms.add('match:any_room');
+
+    server.sockets.adapter.rooms.set(
+      'match:any_room',
+      new Set<string>(['any_player_one']),
+    );
+
+    sut.execute(server, client, 'any_word');
+
+    expect(client.to).not.toHaveBeenCalled();
+  });
 });
