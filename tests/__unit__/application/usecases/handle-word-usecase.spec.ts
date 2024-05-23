822af6b41d0efdcd8358125f00cfd4b192b1ac19
RonaldGuilhermePDS <RonaldGuilhermePDS@gmail.com>
2024-05-23 19:28:46 -0300

    test: ensure handle word use case not emit if client is not in a match room

---
+++ tests/__unit__/application/usecases/handle-word-usecase.spec.ts
@@ -0,0 +1,46 @@
+import { Test, TestingModule } from '@nestjs/testing';
+
+import { HandleWordUseCase } from '@/application/usecases/handle-word-usecase';
+import { GameConstants } from '@/main/constants/game-constants';
+import { Server, Socket } from 'socket.io';
+import { vi, describe, it, expect } from 'vitest';
+
+const makeSut = async () => {
+  const module: TestingModule = await Test.createTestingModule({
+    providers: [HandleWordUseCase],
+  }).compile();
+
+  const sut = module.get<HandleWordUseCase>(HandleWordUseCase);
+
+  return { sut };
+};
+
+const makeClientMock = (): Socket =>
+  ({
+    id: 'any_player_one',
+    rooms: new Set<string>(),
+    to: vi.fn().mockReturnThis(),
+    emit: vi.fn(),
+  }) as unknown as Socket;
+
+const makeServerMock = (): Server =>
+  ({
+    sockets: {
+      adapter: {
+        rooms: new Map<string, Set<string>>(),
+      },
+    },
+  }) as unknown as Server;
+
+describe('Handle Word Use Case', () => {
+  it('Should not emit if client is not in a match room', async () => {
+    const { sut } = await makeSut();
+
+    const client = makeClientMock();
+    const server = makeServerMock();
+
+    sut.execute(server, client, 'any_word');
+
+    expect(client.to).not.toHaveBeenCalled();
+  });
+});
