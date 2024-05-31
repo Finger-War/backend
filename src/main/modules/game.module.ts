d4f74d2c488bb4d7ba9cd0d7884932c7dfca48c6
RonaldGuilhermePDS <RonaldGuilhermePDS@gmail.com>
2024-05-31 04:09:51 -0300

    refactor: move game gateway to presentation

--- src/main/modules/game.module.ts
+++ src/main/modules/game.module.ts
@@ -4,11 +4,11 @@ import { GetOutQueueUseCase } from '@/application/usecases/get-out-queue-usecase
 import { HandleCorrectWordUseCase } from '@/application/usecases/handle-correct-word';
 import { HandleWordUseCase } from '@/application/usecases/handle-word-usecase';
 import { JoingGameQueueUseCase } from '@/application/usecases/joing-game-queue-usecase';
-import { GameGateway } from '@/infrastructure/gateways/game-gateway';
 import { InMemoryMatchRepository } from '@/infrastructure/repositories/in-memory-match-repository';
 import { InMemoryQueueRepository } from '@/infrastructure/repositories/in-memory-queue-repository';
 import { MatchMakingService } from '@/infrastructure/services/match-making-service';
 import { WordsService } from '@/infrastructure/services/words-service';
+import { GameGateway } from '@/presentation/gateways/game-gateway';
 
 @Module({
   imports: [],
